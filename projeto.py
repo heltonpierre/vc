aa1c3eb3e9a57ec03ba9a7912465314c6d8057ee
heltonpierre <helton_pierre@hotmail.com>
2021-04-27 20:20:01 -0300

    2021-04-27

--- projeto.py
+++ projeto.py
@@ -1,16 +1,24 @@
 import cv2
-webcam = cv2.VideoCapture(0)
+
+webcam = cv2.VideoCapture(1)
+
+#webcam = cv2.VideoCapture('1.mp4')
+'''
+ip = 'http://192.168.255.112:8080/video'
+webcam = cv2.VideoCapture(ip)
+'''
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 qtd_pessoas = int(0)
+
+fgbg = cv2.createBackgroundSubtractorGMG()
 
 while(True):
     s, video = webcam.read()
 
     video = cv2.flip(video, 180)
 
-    faces = face_cascade.detectMultiScale(
-        video, minNeighbors=20, minSize=(30, 30), maxSize=(400, 400))
+    faces = face_cascade.detectMultiScale(video, minNeighbors=20, minSize=(30, 30), maxSize=(400, 400))
 
     for (x, y, w, h) in faces:
         cv2.rectangle(video, (x, y), (x+w, y+h), (255, 0, 0), 4)
@@ -18,8 +26,11 @@ while(True):
         cv2.putText(video, "Quantidade de Faces: " + contador, (10, 450),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
         print(contador)
-    qtd_pessoas += int(contador)
-    print (f"Número de pessoas detectadas {qtd_pessoas}")
+        pessoas_dectadas = int(contador)
+        print (f"Número de pessoas detectadas {pessoas_dectadas}")
+        qtd_pessoas += pessoas_dectadas
+        pessoas_dectadas = int(0)
+        print (f"Número total de pessoas {qtd_pessoas}")
     
     cv2.imshow("Face Detection", video)
 
