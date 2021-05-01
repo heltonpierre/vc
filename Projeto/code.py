799646d22e5591d671f7be59ebabffb2325a85e4
helton.pierre <helton.pierre@pop-rn.rnp.br>
2021-05-01 08:58:58 -0300

    2021-05-01

--- Projeto/code.py
+++ Projeto/code.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def center(x, y, w, h):
+def calc_centro(x, y, w, h):
     x1 = int(w / 2)
     y1 = int(h / 2)
     cx = x + x1
@@ -21,16 +21,17 @@ ThresholdBinarizacao = 200
 OffsetLinhasRef = 30
 PosL = 200
 
-source = '1.mp4'
+source = 'pessoas.mp4'
+#source = 'http://www.insecam.org/en/view/916089/'
 camera = cv2.VideoCapture(source)
 #forca a camera a ter resolucao 640x480
 camera.set(3,640)
 camera.set(4,480)
 
 #Objeto que extrai o plano fundo do vídeo
-mask_original = cv2.createBackgroundSubtractorMOG2()
+mask_bkgMOG2 = cv2.createBackgroundSubtractorMOG2()
 
-detects = []
+rastreamento = []
 
 while True:
     status, frame = camera.read()
@@ -44,12 +45,11 @@ while True:
         break
     #converte frame para escala de cinza 
     framegray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    cv2.imshow("Imagem Escala de Cinza", framegray)
+    #cv2.imshow("Imagem Escala de Cinza", framegray)
 
     #Gerando a máscara da imagem em escala para realizar a comparação (subtracao de background) - identificar o que está mudando a cada frame.
-    mask_gray = mask_original.apply(frame)
-    cv2.imshow("Máscara Cinza", mask_gray)
+    mask_gray = mask_bkgMOG2.apply(frame)
+    #cv2.imshow("Máscara Cinza", mask_gray)
 
     #Binarizacao do frame com background subtraido (remover ruídos)
     frame_thresh = cv2.threshold(mask_gray, ThresholdBinarizacao, 255, cv2.THRESH_BINARY)[1] #Apenas Preto e Branco
@@ -84,41 +84,46 @@ while True:
     # Extraindo o contorno das massas em movimento.
     contornos_frame, _ = cv2.findContours(frame_dilation.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
-    i = 0
+    id = 0
     for contorno in contornos_frame:
         (x,y,w,h) = cv2.boundingRect(contorno)  #x e y: coordenadas do vertice superior esquerdo
                                                 #w e h: respectivamente largura e altura do retangulo
 
         area = cv2.contourArea(contorno)
         
+        #Verificando o tamanho da área
         if int(area) > AreaContornoLimiteMin:
-            centro = center(x, y, w, h)
-            cv2.putText(frame, str(i), (x+10, y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255),2)
+            centro = calc_centro(x, y, w, h)
+            #cv2.putText(frame, str(id), (x+10, y+20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255),2)
             cv2.circle(frame, centro, 4, (0, 0,255), -1)
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
-            if len(detects) <= i:
-                detects.append([])
+
+            if len(rastreamento) <= id:
+                rastreamento.append([])
+
             if centro[1]> CoordenadaYLinhaCentro - OffsetLinhasRef and centro[1] < CoordenadaYLinhaCentro + OffsetLinhasRef:
-                detects[i].append(centro)
+                rastreamento[id].append(centro)
+
             else:
-                detects[i].clear()
-            i += 1
+                rastreamento[id].clear()
+            id += 1
 
-    if i == 0:
-        detects.clear()
+    if id == 0:
+        rastreamento.clear()
 
-    i = 0
+    id = 0
 
     if len(contornos_frame) == 0:
-        detects.clear()
+        rastreamento.clear()
 
     else:
 
-        for detect in detects:
+        for detect in rastreamento:
             for (c,l) in enumerate(detect):
+                #print(f'Coluna: {c} e Linha: {l}')
 
-
-                if detect[c-1][1] < CoordenadaYLinhaCentro and l[1] > CoordenadaYLinhaCentro :
+                if detect[c-1][1] < CoordenadaYLinhaCentro and l[1] > CoordenadaYLinhaCentro:
+                    #print(f'detect[c-1][1]= {detect[c-1][1]} CoordenadaYLinhaCentro = {CoordenadaYLinhaCentro} l[1] = {l[1]}')
                     detect.clear()
                     saindo+=1
                     total+=1
@@ -126,6 +131,7 @@ while True:
                     continue
 
                 if detect[c-1][1] > CoordenadaYLinhaCentro and l[1] < CoordenadaYLinhaCentro:
+                    #print(f'detect[c-1][1]= {detect[c-1][1]} CoordenadaYLinhaCentro = {CoordenadaYLinhaCentro} l[1] = {l[1]}')
                     detect.clear()
                     entrando+=1
                     total+=1
@@ -137,7 +143,7 @@ while True:
 
     cv2.putText(frame, "TOTAL PASSARAM: "+str(total), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255),2)
     cv2.putText(frame, "TOTAL DENTRO: "+str(entrando - saindo), (10, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255),2)
-    cv2.putText(frame, "SAINDO: "+str(saindo), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0),2)
+    cv2.putText(frame, "SAINDO: "+str(saindo), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0),2)
     cv2.putText(frame, "ENTRADO: "+str(entrando), (10, 80), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255),2)
 
     cv2.imshow("frame", frame)
